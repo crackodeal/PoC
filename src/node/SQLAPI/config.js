2af2a1f7238b72047796bdafbc03368a0e50f85e
Yury Bobrov <79133480+crackodeal@users.noreply.github.com>
2024-06-10 08:59:12 +0300

    Update config.js

--- src/node/SQLAPI/config.js
+++ src/node/SQLAPI/config.js
@@ -1,7 +1,7 @@
 ﻿var config = {
   production: {
      driver: 'msnodesqlv8',
-     connectionString: 'Server=ubuntu22.bobrof.info,1433;Database=TODS_DEMO;UID=tods;PWD=II@E$tt2rjJm;Driver={SQL Server Native Client 11.0}'
+     connectionString: 'Server=ubuntu22.bobrof.info,1433;Database=TODS_DEMO;UID=tods;PWD=II@E$tt2rjJm;Driver={ODBC Driver 17 for SQL Server}'
      } 
 };
 module.exports = config;
